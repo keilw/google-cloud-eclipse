30cef2eb712ca2b02a7d025586e81b6da64951d0
Andras Kerekes <akerekes@users.noreply.github.com>
2017-03-22 22:50:49 -0400

    Update continuous.sh

--- kokoro/ubuntu/continuous.sh
+++ kokoro/ubuntu/continuous.sh
@@ -5,5 +5,13 @@ set -e
 # Display commands being run.
 set -x
 
+CLOUDSDK_CORE_DISABLE_USAGE_REPORTING=true
+DISPLAY=:99.0
+
+sh -e /etc/init.d/xvfb start
+sleep 3 # give xvfb some time to start
+metacity --sm-disable --replace &
+sleep 3 
+
 cd github/google-cloud-eclipse
 mvn -Ptravis --fail-at-end verify
